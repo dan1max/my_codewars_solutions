4e4b87e2f6409962384dd5467b5043174f41217c
dan1max <154277102+dan1max@users.noreply.github.com>
2026-07-16 19:06:03 +0200

    u

---
+++ C/rgb_to_hex.c
@@ -0,0 +1,26 @@
+/*
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+Examples (input --> output):
+255, 255, 255 --> "FFFFFF"
+255, 255, 300 --> "FFFFFF"
+0, 0, 0       --> "000000"
+148, 0, 211   --> "9400D3"
+*/
+
+//My solution:
+
+#include <stdio.h>
+void rgb (int r, int g, int b, char hex[6 + 1]){
+  if(r>255)r=255;
+  if(g>255)g=255;
+  if(b>255)b=255;
+  
+  if(r<0)r=0;
+  if(g<0)g=0;
+  if(b<0)b=0;
+
+  sprintf(hex, "%02X%02X%02X", r, g, b);
+}
